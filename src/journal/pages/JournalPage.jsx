25c13687f28bc901b760114a77643bd0db70e57b
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-02 14:03:18 -0700

    JournalPage.jsx Boton flotante Implementado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 
 export const JournalPage = () => {
@@ -9,8 +11,23 @@ export const JournalPage = () => {
       
       {/* <Typography>Amet et exercitation laborum nostrud laborum quis consectetur consectetur Lorem excepteur culpa nostrud commodo. Ea duis ex aliqua nisi voluptate dolore adipisicing in duis id. Do in ex aute cupidatat ut aute commodo ad pariatur ad in voluptate. Adipisicing duis tempor duis sit et qui non nisi cillum in sint duis. Eiusmod est non cupidatat ipsum aute est labore do adipisicing cupidatat deserunt. </Typography> */}
     
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/*  <NoteView />  */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: 'error.main', opacity: 0.7 },
+          position: "fixed",
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
 
     </JournalLayout>
   )
