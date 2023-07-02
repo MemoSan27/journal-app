fe215682777f5b84aabfeeec5092d8dac0205b9a
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-02 13:45:07 -0700

    NoteView.jsx Implementado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 
 export const JournalPage = () => {
@@ -9,8 +9,8 @@ export const JournalPage = () => {
       
       {/* <Typography>Amet et exercitation laborum nostrud laborum quis consectetur consectetur Lorem excepteur culpa nostrud commodo. Ea duis ex aliqua nisi voluptate dolore adipisicing in duis id. Do in ex aute cupidatat ut aute commodo ad pariatur ad in voluptate. Adipisicing duis tempor duis sit et qui non nisi cillum in sint duis. Eiusmod est non cupidatat ipsum aute est labore do adipisicing cupidatat deserunt. </Typography> */}
     
-      <NothingSelectedView />
-      {/*NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
     </JournalLayout>
   )
