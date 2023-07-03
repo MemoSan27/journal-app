2c5cd8b19cab65feab079f7c9fd08ae20de55f4e
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-03 13:46:31 -0700

    authSlice.js Autenticacion por medio de google implementado

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,13 +1,16 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
 import { checkingAuthentication, startGoogleSingIn } from '../../store/auth';
+import { useMemo } from 'react';
 
 
 export const LoginPage = () => {
+  
+  const { status } = useSelector( state => state.auth);
 
   const dispatch = useDispatch();
 
@@ -15,6 +18,8 @@ export const LoginPage = () => {
     email: 'memo@google.com',
     password: '123456'
   });
+
+  const isAuthenticating = useMemo( () => status === 'checking', [status]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -58,13 +63,19 @@ export const LoginPage = () => {
 
               <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1}} >
                 <Grid item xs={ 12 } sm={ 6 }>
-                  <Button type="submit" variant="contained" fullWidth>
+                  <Button 
+                    disabled={ isAuthenticating }
+                    type="submit" 
+                    variant="contained" 
+                    fullWidth
+                    >
                     Login
                   </Button>
                 </Grid>
 
                 <Grid item xs={ 12 } sm={ 6 }>
                   <Button 
+                    disabled={ isAuthenticating }
                     onClick={ onGoogleSignIn }
                     variant="contained" 
                     fullWidth
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
-import { checkingCredentials } from './'
+import { signInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, login, logout } from './'
 
 export const checkingAuthentication = ( email, password ) => {
     return async( dispatch ) => {
@@ -12,6 +13,11 @@ export const startGoogleSingIn = () => {
     return async( dispatch ) => {
 
         dispatch( checkingCredentials() );
+        const result = await signInWithGoogle();
+        
+        if( !result.ok ) return dispatch( logout( result.errorMessage));
+
+        dispatch( login( result ) )
        
     }
 }
