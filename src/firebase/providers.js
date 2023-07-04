02de0767ed59ad33b41c02a78eb271d3c1a0a7e1
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-03 18:03:37 -0700

    thunks.js Usuario autenticado con email y password

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { FireBaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -27,4 +27,27 @@ export const signInWithGoogle = async() => {
             errorMessage,
         }
    }
+}
+
+
+export const registerUserWithEmailPassword = async({ email, password, displayName }) => {
+    try {
+
+        console.log({ email, password, displayName })
+
+        const resp = await createUserWithEmailAndPassword( FireBaseAuth, email, password );
+        const { uid, photoURL } =  resp.user;
+        //TODO: actualizar el displayname en firebase
+        await updateProfile( FireBaseAuth.currentUser, { displayName });
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+        
+    } catch (error) {
+        console.log(error); 
+        return { ok: false, errorMessage: error.message}
+        
+    }
 }
